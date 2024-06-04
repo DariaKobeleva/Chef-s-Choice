7d83fbe332eb28465d78db8db529af77c2568044
KDaria <dkdva@mail.ru>
2024-06-04 18:52:19 +0200

    Grid in mainView

--- Chef’s Choice/MainView.swift
+++ Chef’s Choice/MainView.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject private var welcomeViewVM: WelcomeViewViewModel
+    
+    private var data = Array(1...20)
+    private let colors: [Color] = [.red, .green, .blue, .yellow, .purple]
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 170))
+    ]
+    
     var body: some View {
-        Text("Hello, \(welcomeViewVM.user.name)")
-        ButtonView(action: welcomeViewVM.logout, text: "Log out", colorButton: .blue)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                    ForEach(data, id: \.self) { number in
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 170, height: 170)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+//                Text("Hello, \(welcomeViewVM.user.name)")
+//                ButtonView(action: welcomeViewVM.logout, text: "Log out", colorButton: .blue)
+            }
+            .navigationTitle("Recipes")
+        }
     }
 }
 

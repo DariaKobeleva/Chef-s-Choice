c00265b4aa311376095ccd1d24fecec13fd69bd0
KDaria <dkdva@mail.ru>
2024-07-11 16:45:53 +0200

    Implemented camera and photo library functionality for profile picture change
    - Added button to select a new profile picture
    - Implemented image selection from camera and photo library
    - Updated profile interface to display the selected image

--- Chef’s Choice/ProfileView.swift
+++ Chef’s Choice/ProfileView.swift
@@ -11,31 +11,53 @@ import PhotosUI
 struct ProfileView: View {
     @EnvironmentObject private var welcomeViewVM: WelcomeViewViewModel
     @State private var profileImage: UIImage?
-    @State private var photosPickerItem: PhotosPickerItem?
+    @State private var showingImagePicker = false
+    @State private var showingActionSheet = false
+    @State private var imagePickerSourceType: UIImagePickerController.SourceType = .photoLibrary
     
     var body: some View {
         NavigationStack {
             VStack {
                 Image(uiImage: profileImage ?? UIImage(named: "catChef")!)
                     .resizable()
+                    .scaledToFill()
                     .frame(width: 200, height: 200)
-                    .aspectRatio(contentMode: .fit)
-                    .shadow(radius: 5)
                     .clipShape(Circle())
+                    .shadow(radius: 5)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
                 
-                PhotosPicker(selection: $photosPickerItem, matching: .images) {
+                Button(action: {
+                    showingActionSheet = true
+                }) {
                     Text("Change Profile Picture")
                         .foregroundColor(.gray)
                         .cornerRadius(10)
                 }
                 .padding()
+                .actionSheet(isPresented: $showingActionSheet) {
+                    ActionSheet(
+                        title: Text("Select Photo"),
+                        message: nil,
+                        buttons: [
+                            .default(Text("Camera")) {
+                                imagePickerSourceType = .camera
+                                showingImagePicker = true
+                            },
+                            .default(Text("Photo Library")) {
+                                imagePickerSourceType = .photoLibrary
+                                showingImagePicker = true
+                            },
+                            .cancel()
+                        ]
+                    )
+                }
+                
                 List {
                     TextField("Enter your name", text: $welcomeViewVM.user.name)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding(.vertical, 5)
                 }
             }
-            
             .navigationTitle("Chef \(welcomeViewVM.user.name)")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
@@ -45,20 +67,11 @@ struct ProfileView: View {
                 }
             }
         }
-        .onChange(of: photosPickerItem) { _, _ in
-            Task {
-                if let photosPickerItem,
-                   let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
-                    if let image = UIImage(data: data) {
-                        profileImage = image
-                    }
-                }
-                photosPickerItem = nil
-            }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(isPresented: $showingImagePicker, image: $profileImage, sourceType: imagePickerSourceType)
         }
     }
 }
-
 
 #Preview {
     ProfileView()
